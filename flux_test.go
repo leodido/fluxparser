311c76ea57306ace1339279350b5f4b9e0434de6
Leonardo Di Donato <leodidonato@gmail.com>
2018-09-21 21:19:04 -0700

    New: Testing some duration literals

--- flux_test.go
+++ flux_test.go
@@ -27,6 +27,15 @@ func TestParse(t *testing.T) {
 		// 	name: "nest block statements",
 		// 	raw:  `{{}}`,
 		// },
+		// {
+		// 	name: "variable declaration",
+		// 	raw:  `leodido = `,
+		// },
+		// {
+		// 	name: "variable declaration",
+		// 	raw:  `{docmerlin = "emrys"}`,
+		// },
+
 		{
 			name: "use variable to declare something",
 			raw:  `howdy = "literal"`,
@@ -41,29 +50,100 @@ func TestParse(t *testing.T) {
 				},
 			},
 		},
-
 		// {
-		// 	name: "variable declaration",
-		// 	raw:  `leodido = `,
-		// },
-		// {
-		// 	name: "variable declaration",
-		// 	raw:  `{docmerlin =}`,
+		// 	name: "from function",
+		// 	raw:  `from()`,
+		// 	want: &ast.Program{
+		// 		Body: []ast.Statement{
+		// 			&ast.ExpressionStatement{
+		// 				Expression: &ast.CallExpression{
+		// 					Callee: &ast.Identifier{
+		// 						Name: "from",
+		// 					},
+		// 				},
+		// 			},
+		// 		},
+		// 	},
 		// },
 		{
-			name: "from function",
-			raw:  `from()`,
+			name: "duration literal, all units",
+			raw:  `dur = 1y3mo2w1d4h1m30s5ms2μs70ns`,
 			want: &ast.Program{
-				Body: []ast.Statement{
-					&ast.ExpressionStatement{
-						Expression: &ast.CallExpression{
-							Callee: &ast.Identifier{
-								Name: "from",
+				Body: []ast.Statement{&ast.VariableDeclaration{
+					Declarations: []*ast.VariableDeclarator{{
+						ID: &ast.Identifier{Name: "dur"},
+						Init: &ast.DurationLiteral{
+							Values: []ast.Duration{
+								{Magnitude: 1, Unit: "y"},
+								{Magnitude: 3, Unit: "mo"},
+								{Magnitude: 2, Unit: "w"},
+								{Magnitude: 1, Unit: "d"},
+								{Magnitude: 4, Unit: "h"},
+								{Magnitude: 1, Unit: "m"},
+								{Magnitude: 30, Unit: "s"},
+								{Magnitude: 5, Unit: "ms"},
+								{Magnitude: 2, Unit: "us"},
+								{Magnitude: 70, Unit: "ns"},
 							},
 						},
-					},
-				},
+					}},
+				}},
 			},
+		},
+		{
+			name: "duration literal, months",
+			raw:  `dur = 6mo`,
+			want: &ast.Program{
+				Body: []ast.Statement{&ast.VariableDeclaration{
+					Declarations: []*ast.VariableDeclarator{{
+						ID: &ast.Identifier{Name: "dur"},
+						Init: &ast.DurationLiteral{
+							Values: []ast.Duration{
+								{Magnitude: 6, Unit: "mo"},
+							},
+						},
+					}},
+				}},
+			},
+		},
+		{
+			name: "duration literal, milliseconds",
+			raw:  `dur = 500ms`,
+			want: &ast.Program{
+				Body: []ast.Statement{&ast.VariableDeclaration{
+					Declarations: []*ast.VariableDeclarator{{
+						ID: &ast.Identifier{Name: "dur"},
+						Init: &ast.DurationLiteral{
+							Values: []ast.Duration{
+								{Magnitude: 500, Unit: "ms"},
+							},
+						},
+					}},
+				}},
+			},
+		},
+		{
+			name: "duration literal, months, minutes, milliseconds",
+			raw:  `dur = 6mo30m500ms`,
+			want: &ast.Program{
+				Body: []ast.Statement{&ast.VariableDeclaration{
+					Declarations: []*ast.VariableDeclarator{{
+						ID: &ast.Identifier{Name: "dur"},
+						Init: &ast.DurationLiteral{
+							Values: []ast.Duration{
+								{Magnitude: 6, Unit: "mo"},
+								{Magnitude: 30, Unit: "m"},
+								{Magnitude: 500, Unit: "ms"},
+							},
+						},
+					}},
+				}},
+			},
+		},
+		{
+			name:    "duration literal, units unordered are not allowed",
+			raw:     `dur = 6mo30m3mo`,
+			wantErr: true,
 		},
 	}
 
